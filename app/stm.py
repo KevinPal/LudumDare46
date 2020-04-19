c5cb5d4b98cc9d0586771b297068da7d95616bcd
Kevin Zheng <coffeevector@gmail.com>
2020-04-18 18:40:08 -0700

    stm-tv: Adds vstm-tv
    This function takes a matrix-valued function, a time frame, and a
    step-size to compute state transition matrices from t_0 to t in [t_0,
    t_f]
    
    Dot product between these state transition matrices and the initial
    vector will product a stroboscopic view of the final vector

--- app/stm.py
+++ app/stm.py
@@ -24,3 +24,29 @@ def test_stm_ti():
     plt.plot(np.linspace(0, 1, n), [np.dot(o[1], x_0) for o in out])
     plt.show()
     return out
+
+def vstm_tv(A, t_f, t_0, n=1000):
+    # A is callable which returns an array
+    l = len(A(t_0))
+    t_span = np.linspace(t_0, t_f, num=n)
+    delta_t = (t_f - t_0)/n
+    phi = [expm(A(t) * delta_t) for t in t_span]
+    stm = [np.identity(l)]
+    for p in phi:
+        stm.append(np.dot(p, stm[-1]))
+
+    return stm
+
+def test_vstm_tv():
+    import matplotlib.pyplot as plt
+    A = lambda t: np.array([
+        [0, 1],
+        [-t, 0]
+    ])
+
+    x_0 = np.array([0.5, 0])
+    t_0 = 0
+    t_f = 3 * np.pi
+
+    n = 600
+    return vstm_tv(A, t_f, t_0, n)[1:]
